cec515347c0e5562e4a63af11d95a1e1cfaa33c5
Kamil Lasecki <Kamil Lasecki>
2016-02-28 23:58:08 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding AfterBuild='deploy' />
+var gulp = require('gulp');
+var deploy = require('ssh-deploy');
+var Candyman = new require('candyman');
+var candyman = new Candyman({
+    targetDevices: [
+        { devicename: 'galileo', hostname: '192.168.0.106' }
+    ],
+    projectName: 'webTemp',
+    user: 'root',
+    password: '',
+    startfile: 'app.js'
+});
+
+gulp.task('deploy', function () {
+//return candyman.deploy();
+    deploy.publish('root@192.168.0.106', { directory: process.cwd }).then(function () {
+        return deploy.start('root@192.168.0.106', { directory: process.cwd });
+    }).done(function () {
+        console.log('updated deployment');
+    });
+});
