f25f4ebcf7ace505f16a65af3cbe77a696d1c6c0
Kamil Lasecki <Kamil Lasecki>
2016-02-29 01:50:46 +0000

    2

--- actions/mesure.js
+++ actions/mesure.js
@@ -15,7 +15,7 @@ exports.get = function (req, res){
         var voltage = reading * 4.202312139;
                 
         //var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
-        console.log("Resistance: " + resistance);
+        //console.log("Resistance: " + resistance);
 
         var celsius_temperature = (voltage - 500) / 10;
         console.log("Analog Pin (A0) Output: " + Math.round(reading * 100) / 100 + "    |    jump: " + Math.round(jump * 100) / 100 + "    |     Voltage: " + Math.round(voltage * 100) / 100 + "    |    Celsius Temperature " + Math.round(celsius_temperature * 100) / 100);
