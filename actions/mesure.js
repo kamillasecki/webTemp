554aa6b65f9d87e049bb0df788d5f4305e4c8b3e
Kamil Lasecki <Kamil Lasecki>
2016-02-29 01:24:11 +0000

    2

---
+++ actions/mesure.js
@@ -0,0 +1,35 @@
+﻿var mesure = function () {
+    var get = function (req, res){
+        console.log('Hello world');
+        var mraa = require('mraa');
+        
+        startSensorWatch();
+        //toggle();
+        
+        var inputV = 4.3;
+        var inputmV = inputV * 1000;
+        
+        //GROVE Kit A0 Connector --> Aio(0)
+        var myAnalogPin = new mraa.Aio(1);
+
+           
+        var reading = myAnalogPin.read();
+        var jump = (inputmV / 1024);
+        var voltage = reading * 4.202312139;
+                
+        var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
+        console.log("Resistance: " + resistance);
+
+        var celsius_temperature = (voltage - 500) / 10;
+        console.log("Analog Pin (A0) Output: " + Math.round(reading * 100) / 100 + "    |    jump: " + Math.round(jump * 100) / 100 + "    |     Voltage: " + Math.round(voltage * 100) / 100 + "    |    Celsius Temperature " + Math.round(celsius_temperature * 100) / 100);
+        res.send("temperature now: " + celsius_temperature);
+    }
+    
+
+    return {
+        get: get,
+
+    };
+};
+
+module.exports = mesure;
--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 ﻿//Lets require/import the HTTP module
 var http = require('http');
+var express = require('express');
+var app = express();
+var port = 8080;
+var mesure = require('./actions/mesure.js')
 
 //Lets define a port we want to listen to
 const PORT=8080;
 
 //We need a function which handles requests and send response
-function handleRequest(request, response) {
-    response.end('It Works!! Path Hit: ' + request.url);
-}
 
-//Create a server
-var server = http.createServer(handleRequest);
+
+app.get('/', function (req, res) {
+    return mesure.get(req, res);
+});
 
 //Lets start our server
-server.listen(PORT, function () {
+app.listen(PORT, function () {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
