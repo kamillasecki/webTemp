c169115bfec6b07413c504d3a6e94ba81c6a79c3
kamillasecki <kamil.lasecki@eircom.net>
2016-03-08 00:17:13 +0000

    update

--- actions/mesure.js
+++ actions/mesure.js
@@ -2,45 +2,52 @@
 exports.get = function (req, res){
         console.log('Hello world');
         var five = require("johnny-five");
-        var Galileo = require("galileo-io");
+        //var Galileo = require("galileo-io");
 
-        var board = new five.Board({
-                io: new Galileo()
-        });
+        //var board = new five.Board({
+                //io: new Galileo()
+        //});
  
-        var temp = getTemperature();
+        //var temp = getTemperature();
         
-        function getTemperature() {
+        var pin = new five.Pin("A0");
 
-                var THERMISTORNOMINAL = 10000;
-                var TEMPERATURENOMINAL = 25;
-                var BCOEFFICIENT = 3950;
-                var SERIESRESISTOR = 10000 ;
-                board.pinMode("A0", five.Pin.OUTPUT);
-                var reading = board.analogRead("A0", function(value) {
-                        console.log("Value: " + value);
-                        var r = value;
-                        return r});
+        var temp = pin.read(function(error, value) {
+          console.log(value);
+          return value;
+        });
+        
+        // function getTemperature() {
+
+        //         var THERMISTORNOMINAL = 10000;
+        //         var TEMPERATURENOMINAL = 25;
+        //         var BCOEFFICIENT = 3950;
+        //         var SERIESRESISTOR = 10000 ;
+        //         board.pinMode("A0", five.Pin.OUTPUT);
+        //         var reading = board.analogRead("A0", function(value) {
+        //                 console.log("Value: " + value);
+        //                 var r = value;
+        //                 return r});
                         
-                console.log("Reading: " + JSON.stringify(reading));
+        //         console.log("Reading: " + JSON.stringify(reading));
                 
                         
-                var voltage = (reading * 5.0) / 1023;
-                var resistance = 1023/reading;
-                resistance = resistance - 1;
-                resistance = 8800 / resistance;
+        //         var voltage = (reading * 5.0) / 1023;
+        //         var resistance = 1023/reading;
+        //         resistance = resistance - 1;
+        //         resistance = 8800 / resistance;
                 
-                var steinhart = resistance / THERMISTORNOMINAL;     // (R/Ro)
-                steinhart = Math.log(steinhart);                  // ln(R/Ro)
-                steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-                steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-                steinhart = 1.0 / steinhart;                 // Invert
-                steinhart -= 273.15;                         // convert to C
+        //         var steinhart = resistance / THERMISTORNOMINAL;     // (R/Ro)
+        //         steinhart = Math.log(steinhart);                  // ln(R/Ro)
+        //         steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+        //         steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+        //         steinhart = 1.0 / steinhart;                 // Invert
+        //         steinhart -= 273.15;                         // convert to C
 
-                console.log("Value: " + reading + "   |   voltage: " + voltage + "   |   Resistance: " + resistance + "   |   temp: " + steinhart);
+        //         console.log("Value: " + reading + "   |   voltage: " + voltage + "   |   Resistance: " + resistance + "   |   temp: " + steinhart);
                         
-                return steinhart;
-        }
+        //         return steinhart;
+        // }
         
 
 
