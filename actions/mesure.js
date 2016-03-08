653474452ac04f84ab3c824086f93a198048e228
kamillasecki <kamil.lasecki@eircom.net>
2016-03-08 01:40:26 +0000

    update

--- actions/mesure.js
+++ actions/mesure.js
@@ -2,16 +2,36 @@
 exports.get = function (req, res){
         var r;
         console.log('Hello world');
-        var cylon = require("cylon");
-        cylon.robot({
-                name:"galileo",
-                connections: {galileo: { adaptor: 'intel-iot'} },
-                devices: {pin: {driver: 'direct-pin' , pin: "a0" } },
-                work: function (galileo) {
-                        r = galileo.pin.analogRead();
-                        console.log(r);
-                }
-        }).start();
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+
+  devices: {
+    sensor: { driver: 'analog-sensor', pin: 0, lowerLimit: 100, upperLimit: 900 }
+  },
+
+  work: function(my) {
+    var analogValue = 0;
+
+    every((1).second(), function() {
+      analogValue = my.sensor.analogRead();
+      console.log('Analog value => ', analogValue);
+    });
+
+    my.sensor.on('lowerLimit', function(val) {
+      console.log("Lower limit reached!");
+      console.log('Analog value => ', val);
+    });
+
+    my.sensor.on('upperLimit', function(val) {
+      console.log("Upper limit reached!");
+      console.log('Analog value => ', val);
+    });
+  }
+}).start();
         
         
         //var Galileo = require("galileo-io");
