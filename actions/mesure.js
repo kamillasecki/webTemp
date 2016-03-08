571e3dddf1e51ec31aae0bf8fdf36deae5f60ef9
kamillasecki <kamil.lasecki@eircom.net>
2016-03-08 02:24:19 +0000

    update

--- actions/mesure.js
+++ actions/mesure.js
@@ -34,14 +34,31 @@ exports.get = function(req, res) {
                         var analogValue = 0; //variable for LDR input
 
                         var servoValue = 0; //variable for servo position
-
+                        var THERMISTORNOMINAL = 10000;
+                        var TEMPERATURENOMINAL = 25;
+                        var BCOEFFICIENT = 3950;
+                        var SERIESRESISTOR = 10000 ;
                         my.servo.angle(servoValue); //servo position
 
                         every((1).second(), function() { //determine how often to run
 
                                 analogValue = my.sensor.analogRead(); //read LDR value
+                                
+                                                var voltage = (analogValue * 5.0) / 1023;
+                var resistance = 1023/analogValue;
+                resistance = resistance - 1;
+                resistance = 8800 / resistance;
 
-                                console.log('Analog value => ', analogValue); //write LDR value to terminal
+                var steinhart = resistance / THERMISTORNOMINAL;     // (R/Ro)
+                steinhart = Math.log(steinhart);                  // ln(R/Ro)
+                steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+                steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+                steinhart = 1.0 / steinhart;                 // Invert
+                steinhart -= 273.15;                         // convert to C
+
+                console.log("Value: " + analogValue + "   |   voltage: " + voltage + "   |   Resistance: " + resistance + "   |   temp: " + steinhart);
+
+                               // console.log('Analog value => ', analogValue); //write LDR value to terminal
 
                                 servoValue = ((-analogValue + 1023) * (180 / 1023)); //map LDR values 0-1023 to servo-friendly 0$
 
@@ -60,52 +77,37 @@ exports.get = function(req, res) {
         }).start();
 
 
-        //var Galileo = require("galileo-io");
+  function getTemperature() {
 
-        //var board = new five.Board({
-        //io: new Galileo()
-        //});
+                var THERMISTORNOMINAL = 10000;
+                var TEMPERATURENOMINAL = 25;
+                var BCOEFFICIENT = 3950;
+                var SERIESRESISTOR = 10000 ;
+              
+                var reading = board.analogRead("A0", function(value) {
+                        console.log("Value: " + value);
+                        var r = value;
+                        return r});
 
-        //var temp = getTemperature();
-
-        //var pin = new five.Pin("A0");
-
-        //var temp = pin.read(function(error, value) {
-        //  console.log(value);
-        //  return value;
-        //});
-
-        // function getTemperature() {
-
-        //         var THERMISTORNOMINAL = 10000;
-        //         var TEMPERATURENOMINAL = 25;
-        //         var BCOEFFICIENT = 3950;
-        //         var SERIESRESISTOR = 10000 ;
-        //         board.pinMode("A0", five.Pin.OUTPUT);
-        //         var reading = board.analogRead("A0", function(value) {
-        //                 console.log("Value: " + value);
-        //                 var r = value;
-        //                 return r});
-
-        //         console.log("Reading: " + JSON.stringify(reading));
+                console.log("Reading: " + JSON.stringify(reading));
 
 
-        //         var voltage = (reading * 5.0) / 1023;
-        //         var resistance = 1023/reading;
-        //         resistance = resistance - 1;
-        //         resistance = 8800 / resistance;
+                var voltage = (reading * 5.0) / 1023;
+                var resistance = 1023/reading;
+                resistance = resistance - 1;
+                resistance = 8800 / resistance;
 
-        //         var steinhart = resistance / THERMISTORNOMINAL;     // (R/Ro)
-        //         steinhart = Math.log(steinhart);                  // ln(R/Ro)
-        //         steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-        //         steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-        //         steinhart = 1.0 / steinhart;                 // Invert
-        //         steinhart -= 273.15;                         // convert to C
+                var steinhart = resistance / THERMISTORNOMINAL;     // (R/Ro)
+                steinhart = Math.log(steinhart);                  // ln(R/Ro)
+                steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+                steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+                steinhart = 1.0 / steinhart;                 // Invert
+                steinhart -= 273.15;                         // convert to C
 
-        //         console.log("Value: " + reading + "   |   voltage: " + voltage + "   |   Resistance: " + resistance + "   |   temp: " + steinhart);
+                console.log("Value: " + reading + "   |   voltage: " + voltage + "   |   Resistance: " + resistance + "   |   temp: " + steinhart);
 
-        //         return steinhart;
-        // }
+                return steinhart;
+        }
 
 
 
